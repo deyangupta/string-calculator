99bf1052677c536e0a5e7038c573bce640503f68
deyan gupta <deyan.gupta@deyans-MacBook-Air.local>
2025-03-28 18:11:26 +0530

    Initial project setup with basic React and TypeScript configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StringCalculator from './components/StringCalculator';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <StringCalculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StringCalculator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StringCalculator: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">String Calculator</h1>
+    </div>
+  );
+};
+
+export default StringCalculator;
